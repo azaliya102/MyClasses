f33db9c881378adc33a54f315ac779e2771777c0
azaliya02 <az.muratova2017@yandex.ru>
2024-09-29 01:12:25 +0300

    deleted password

---
+++ src/main/java/email/sending/EmailMessage.java
@@ -0,0 +1,49 @@
+package email.sending;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.Properties;
+
+public class EmailMessage {
+    private String to;
+    private String from;
+    private String subject;
+    private String messageBody;
+    private SMTPConfig smtpConfig;
+
+    public EmailMessage(String to, String from, String subject, String messageBody, SMTPConfig smtpConfig) {
+        this.to = to;
+        this.from = from;
+        this.subject = subject;
+        this.messageBody = messageBody;
+        this.smtpConfig = smtpConfig;
+    }
+
+    private Session createSession() {
+        Properties properties = smtpConfig.getProperties();
+        Authenticator authenticator = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(smtpConfig.getUsername(), smtpConfig.getPassword());
+            }
+        };
+        return Session.getDefaultInstance(properties, authenticator);
+    }
+
+    public MimeMessage createMessage(Session session) throws MessagingException {
+        MimeMessage message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(from));
+        message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        message.setSubject(subject);
+        message.setText(messageBody);
+        return message;
+    }
+
+    public void sendEmail() throws MessagingException {
+        Session session = createSession();
+        MimeMessage message = createMessage(session);
+        Transport.send(message);
+    }
+
+}
+
---
+++ src/main/java/email/sending/Main.java
@@ -0,0 +1,19 @@
+package email.sending;
+
+import javax.mail.MessagingException;
+
+public class Main {
+    public static void main(String[] args) throws MessagingException {
+        SMTPConfig smtpConfig = new SMTPConfig("smtp.gmail.com",
+                587,
+                "azaliamuratova628@gmail.com",
+                "");
+        EmailMessage emailMessage = new EmailMessage("indonesianwesleysnipes@gmail.com",
+                "azaliamuratova628@gmail.com",
+                "test subject",
+                "hello from me!",
+                smtpConfig);
+        emailMessage.sendEmail();
+    }
+
+}
